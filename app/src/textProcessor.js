9b36452e59f5ca15983a5e57af5ede5fecb9b817
Maximiliano de Oliveira Furtado <maximiliano.furtado.ext@dasa.com.br>
2021-02-17 08:36:47 -0300

    feat: extractPeopleData with test

--- app/src/textProcessor.js
+++ app/src/textProcessor.js
@@ -3,11 +3,13 @@ class TextProcess {
   #content
 
   constructor(content){
-    this.content = content
+    this.#content = content
   }
 
   extractPeopleData(){
-
+    const matchPerson = /(?<=[contratante|contratada]:\s{1})(?!\s)(.*\n.*?)$/gmi
+    const onlyPerson = this.#content.match(matchPerson)
+    this.#content = onlyPerson
     return this
   }
 
